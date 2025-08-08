77865a011a2d467b91a198145eea883afbfe0992
matthew20103 <ma77hew20103@gmail.com>
2025-08-08 14:51:47 +0800

    Update PrismaAIRSCredentials.ts

--- credentials/PrismaAIRSCredentials.ts
+++ credentials/PrismaAIRSCredentials.ts
@@ -1,20 +1,31 @@
 import {
-  ICredentialType,
-  INodeProperties,
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
 } from 'n8n-workflow';
 
-export class PrismaAIRSCredential implements ICredentialType {
-  name = 'prismaAIRSCredential';
+export class PrismaAIRSCredentials implements ICredentialType {
+  name = 'prismaAIRSCredentials';
   displayName = 'Prisma AIRS API';
+
+	documentationUrl = 'https://pan.dev/airs/';
+  
   properties: INodeProperties[] = [
     {
       displayName: 'API Key',
       name: 'apiKey',
       type: 'string',
-      typeOptions: { password: true },
       default: '',
-      required: true,
       description: 'Your Palo Alto Networks AIRS API Key.',
     },
   ];
+  authenticate = {
+		type: 'generic',
+		properties: {
+			qs: {
+				'api_key': '={{$credentials.apiKey}}'
+			}
+		},
+	} as IAuthenticateGeneric;
 }
