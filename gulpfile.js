41614f8919578e0c4936392b59e4dfbeccee381f
matthew20103 <ma77hew20103@gmail.com>
2025-08-10 23:48:22 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,6 @@ function copyIcons() {
 
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
-	
-	console.log('Cred Source:', credSource); // <-- And this line
-  console.log('Cred Destination:', credDestination); // <-- And this line
 
 	return src(credSource).pipe(dest(credDestination));
 }
