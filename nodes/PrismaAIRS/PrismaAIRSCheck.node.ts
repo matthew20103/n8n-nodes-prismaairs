5e589a062894dd2b90f4dcb8b50ba758f2ef7014
matthew20103 <ma77hew20103@gmail.com>
2025-08-10 22:42:14 +0800

    Update PrismaAIRSCheck.node.ts

--- nodes/PrismaAIRS/PrismaAIRSCheck.node.ts
+++ nodes/PrismaAIRS/PrismaAIRSCheck.node.ts
@@ -101,38 +101,63 @@ export class PrismaAIRSCheck implements INodeType {
 				displayName: 'Session ID',
 				name: 'sessionId',
 				type: 'string',
-				default: '',
+				default: '{{ $json.sessionId }}',
 				description: 'Unique identifier for the current chat session.',
-				hint: '{{ $json.sessionId }} or {{ $json.output }}',
 				displayOptions: {
 					show: {
-						operation: ['Prisma AIRS Prompt Inspection', 'Prisma AIRS Response Inspection'],
+						operation: ['Prisma AIRS Prompt Inspection'],
 					},
 				},
 			},
 			{
-				displayName: 'Prompt / Response',
+				displayName: 'Prompt',
 				name: 'chatInput',
 				type: 'string',
 				default: '={{ $json.chatInput }}',
-				description: 'The user\'s prompt text to be chcked',
+				description: 'The user\'s prompt text to be checked',
 				required: true,
 				displayOptions: {
 					show: {
-						operation: ['Prisma AIRS Prompt Inspection', 'Prisma AIRS Response Inspection'],
+						operation: ['Prisma AIRS Prompt Inspection'],
 					},
 				},
 			},
 			{
-				displayName: 'Prisma AIRS AI Profile Name',
-				name: 'aiProfileName',
+				displayName: 'Response',
+				name: 'outPut',
 				type: 'string',
-				default: 'Demo-Profile-for-Input',
-				description: 'The Prisma AIRS AI profile name configured for input / output scanning.',
+				default: '={{ $json.output }}',
+				description: 'The AI Agent\'s response text to be checked',
 				required: true,
 				displayOptions: {
 					show: {
-						operation: ['Prisma AIRS Prompt Inspection', 'Prisma AIRS Response Inspection'],
+						operation: ['Prisma AIRS Response Inspection'],
+					},
+				},
+			},
+			{
+				displayName: 'Prisma AIRS AI Profile for Input',
+				name: 'aiProfileNameInput',
+				type: 'string',
+				default: 'Demo-Profile-for-Input',
+				description: 'The Prisma AIRS AI profile name configured for input scanning.',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['Prisma AIRS Prompt Inspection'],
+					},
+				},
+			},
+			{
+				displayName: 'Prisma AIRS AI Profile for Output',
+				name: 'aiProfileNameOutput',
+				type: 'string',
+				default: 'Demo-Profile-for-Output',
+				description: 'The Prisma AIRS AI profile name configured for output scanning.',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['Prisma AIRS Response Inspection'],
 					},
 				},
 			},
@@ -224,147 +249,273 @@ export class PrismaAIRSCheck implements INodeType {
     const returnData: INodeExecutionData[] = [];
 
 		if (items[0].json.hasOwnProperty('prismaAIRSAction')) {
-			const prismaAIRSAction = items[0].json.prismaAIRSAction;
-			const message = items[0].json.chatInput;
-			const sessionId = items[0].json.sessionId;
-			
-			switch (prismaAIRSAction) {
-				case 'allow':
-						// For the case of handling Prisma AIRS Response Inspection, only return output as json key.
-						if (items[0].json.hasOwnProperty('output')) {
+			if (items[0].json.hasOwnProperty('output')) {
+				const message = items[0].json.output;
+					switch (prismaAIRSAction) {
+						case 'allow':
 							returnData.push({
 								json: {
 									output: message,
 								}
 							});
 							return this.prepareOutputData(returnData);
-						}
-						// For the case of handling Prisma AIRS Prompt Inspection, return both sessionId and chatInput as json keys.
-						returnData.push({
-							json: {
-								sessionId: sessionId,
-								chatInput: message,
+							break;
+						case 'block':
+							let messageBlocked = this.getNodeParameter('promptInjectionAttackMessage', 0) as string;
+							interface PromptDetected {
+	       			 	agent?: string;
+	        			injection?: string;
+				        toxic_content?: string;
+				        malicious_code?: string;
+				        url_cats?: string;
+				        dlp?: string;
+				    	}
+							const promptDetected = items[0].json.prompt_detected;
+							if (promptDetected && typeof promptDetected === 'object') {
+								const parsedPrompt = promptDetected as PromptDetected;
+								if (parsedPrompt.agent === 'true') {
+									messageBlocked = this.getNodeParameter('aiAgentAttackMessage', 0) as string;
+								} else if (parsedPrompt.injection === 'true') {
+									messageBlocked = this.getNodeParameter('promptInjectionAttackMessage', 0) as string;
+								} else if (parsedPrompt.toxic_content === 'true') {
+									messageBlocked = this.getNodeParameter('toxicContentMessage', 0) as string;
+								} else if (parsedPrompt.malicious_code === 'true') {
+									messageBlocked = this.getNodeParameter('maliciousCodeMessage', 0) as string;
+								} else if (parsedPrompt.url_cats === 'true') {
+									messageBlocked = this.getNodeParameter('maliciousURLMessage', 0) as string;
+								} else if (parsedPrompt.dlp === 'true') {
+									messageBlocked = this.getNodeParameter('dlpMessage', 0) as string;
+								} else {
+									messageBlocked = parsedPrompt.agent as string;
+								}
 							}
-						});
-					return this.prepareOutputData(returnData);
-					break;
+							returnData.push({
+								json: {
+									output: messageBlocked,
+								}
+							});
+							return this.prepareOutputData(returnData);
+							break;
+						default:
+							returnData.push({
+								json: {
+									output: message,
+									action: 'default',
+								}
+							});
+							return this.prepareOutputData(returnData);
+						}
+			}
+
+			if (items[0].json.hasOwnProperty('chatInput')) {
+				const prismaAIRSAction = items[0].json.prismaAIRSAction;
+				const message = items[0].json.chatInput;
+				const sessionId = items[0].json.sessionId;
+				
+				switch (prismaAIRSAction) {
+					case 'allow':
+							// For the case of handling Prisma AIRS Prompt Inspection, return both sessionId and chatInput as json keys.
+							returnData.push({
+								json: {
+									sessionId: sessionId,
+									chatInput: message,
+								}
+							});
+						return this.prepareOutputData(returnData);
+						break;
 					// For the case where AI attack is found, return the block message to json key output.
-				case 'block':
-						let messageBlocked = this.getNodeParameter('promptInjectionAttackMessage', 0) as string;
-						interface PromptDetected {
-       			 	agent?: string;
-        			injection?: string;
-			        toxic_content?: string;
-			        malicious_code?: string;
-			        url_cats?: string;
-			        dlp?: string;
-			    	}
-						const promptDetected = items[0].json.prompt_detected;
-						if (promptDetected && typeof promptDetected === 'object') {
-							const parsedPrompt = promptDetected as PromptDetected;
-							if (parsedPrompt.agent === 'true') {
-								messageBlocked = this.getNodeParameter('aiAgentAttackMessage', 0) as string;
-							} else if (parsedPrompt.injection === 'true') {
-								messageBlocked = this.getNodeParameter('promptInjectionAttackMessage', 0) as string;
-							} else if (parsedPrompt.toxic_content === 'true') {
-								messageBlocked = this.getNodeParameter('toxicContentMessage', 0) as string;
-							} else if (parsedPrompt.malicious_code === 'true') {
-								messageBlocked = this.getNodeParameter('maliciousCodeMessage', 0) as string;
-							} else if (parsedPrompt.url_cats === 'true') {
-								messageBlocked = this.getNodeParameter('maliciousURLMessage', 0) as string;
-							} else if (parsedPrompt.dlp === 'true') {
-								messageBlocked = this.getNodeParameter('dlpMessage', 0) as string;
-							} else {
-								messageBlocked = parsedPrompt.agent as string;
+					case 'block':
+							let messageBlocked = this.getNodeParameter('promptInjectionAttackMessage', 0) as string;
+							interface PromptDetected {
+	       			 	agent?: string;
+	        			injection?: string;
+				        toxic_content?: string;
+				        malicious_code?: string;
+				        url_cats?: string;
+				        dlp?: string;
+				    	}
+							const promptDetected = items[0].json.prompt_detected;
+							if (promptDetected && typeof promptDetected === 'object') {
+								const parsedPrompt = promptDetected as PromptDetected;
+								if (parsedPrompt.agent === 'true') {
+									messageBlocked = this.getNodeParameter('aiAgentAttackMessage', 0) as string;
+								} else if (parsedPrompt.injection === 'true') {
+									messageBlocked = this.getNodeParameter('promptInjectionAttackMessage', 0) as string;
+								} else if (parsedPrompt.toxic_content === 'true') {
+									messageBlocked = this.getNodeParameter('toxicContentMessage', 0) as string;
+								} else if (parsedPrompt.malicious_code === 'true') {
+									messageBlocked = this.getNodeParameter('maliciousCodeMessage', 0) as string;
+								} else if (parsedPrompt.url_cats === 'true') {
+									messageBlocked = this.getNodeParameter('maliciousURLMessage', 0) as string;
+								} else if (parsedPrompt.dlp === 'true') {
+									messageBlocked = this.getNodeParameter('dlpMessage', 0) as string;
+								} else {
+									messageBlocked = parsedPrompt.agent as string;
+								}
 							}
-						}
+							returnData.push({
+								json: {
+									output: messageBlocked,
+								}
+							});
+						return this.prepareOutputData(returnData);
+						break;
+						// For unknown issue
+					default:
 						returnData.push({
-							json: {
-								output: messageBlocked,
-							}
-						});
-					return this.prepareOutputData(returnData);
-					break;
-					// For unknown issue
-				default:
-					returnData.push({
-							json: {
-								output: 'Unknown issue.',
-							}
-						});
-					return this.prepareOutputData(returnData);
+								json: {
+									output: 'Unknown issue.',
+								}
+							});
+						return this.prepareOutputData(returnData);
+				}
 			}
 		}
 
-		// Proceed only if Prisma AIRS inspection is neeeded
-    for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
-			const sessionId = this.getNodeParameter('sessionId', itemIndex) as string;
-      const chatInput = this.getNodeParameter('chatInput', itemIndex) as string;
-      const aiProfileName = this.getNodeParameter('aiProfileName', itemIndex) as string;
+		// Prisma AIRS Response Inspection
+    if (items[0].json.hasOwnProperty('output') {
+				for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+	      const outPut = this.getNodeParameter('outPut', itemIndex) as string;
+	      const aiProfileNameOutput = this.getNodeParameter('aiProfileNameOutput', itemIndex) as string;
+	
+	      const credentials = await this.getCredentials('prismaAIRSApi') as { apiKey: string };
+	      const apiKey = credentials.apiKey;
+	
+	      const requestOptions: IHttpRequestOptions = {
+	        method: 'POST' as IHttpRequestMethods,
+	        url: 'https://service.api.aisecurity.paloaltonetworks.com/v1/scan/sync/request',
+	        headers: {
+	          'Content-Type': 'application/json',
+	          'x-pan-token': apiKey,
+	        },
+	        body: {
+	          tr_id: '',
+	          ai_profile: {
+	            profile_name: aiProfileNameOutput,
+	          },
+	          metadata: {
+	            app_name: 'n8n',
+	            app_user: 'AI Agent',
+	          },
+	          contents: [
+	            {
+	              prompt: outPut,
+	            },
+	          ],
+	        },
+	        json: true, // Automatically parse JSON response
+	      };
+	
+	      try {
+	        // Pass message to Prisma AIRS API Intercept for inspection
+	        const response = await this.helpers.httpRequest(requestOptions);
+	
+	        // Process the AIRS response
+					const action = response.action;
+					const prompt_detected = response.prompt_detected;
+					returnData.push({
+							json: {
+								output: outPut,
+								prismaAIRSAction: action,
+								prompt_detected: prompt_detected,
+							}
+					});
+					return this.prepareOutputData(returnData);
+	        
+	      } catch (error: unknown) {
+				    let errorMessage = 'Unknown error';
+				    if (error instanceof Error) {
+				        // If the error is a standard Error object, use its message
+				        errorMessage = error.message;
+				    } else if (typeof error === 'object' && error !== null) {
+				        // If it's an object, stringify it to see its contents
+				        errorMessage = JSON.stringify(error);
+				    } else {
+				        // Otherwise, just use its string representation
+				        errorMessage = String(error);
+				    }
+				
+				    console.error('Prisma AIRS API Error:', errorMessage);
+				    returnData.push({
+				        json: {
+				            output: `Error calling Prisma AIRS API: ${errorMessage}`,
+				        },
+				    });
+				}
+			}
+		}
 
-      const credentials = await this.getCredentials('prismaAIRSApi') as { apiKey: string };
-      const apiKey = credentials.apiKey;
-
-      const requestOptions: IHttpRequestOptions = {
-        method: 'POST' as IHttpRequestMethods,
-        url: 'https://service.api.aisecurity.paloaltonetworks.com/v1/scan/sync/request',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-pan-token': apiKey,
-        },
-        body: {
-          tr_id: sessionId,
-          ai_profile: {
-            profile_name: aiProfileName,
-          },
-          metadata: {
-            app_name: 'n8n',
-            app_user: sessionId,
-          },
-          contents: [
-            {
-              prompt: chatInput,
-            },
-          ],
-        },
-        json: true, // Automatically parse JSON response
-      };
-
-      try {
-        // Pass message to Prisma AIRS API Intercept for inspection
-        const response = await this.helpers.httpRequest(requestOptions);
-
-        // Process the AIRS response
-				const action = response.action;
-				const prompt_detected = response.prompt_detected;
-				returnData.push({
-						json: {
-							sessionId: sessionId,
-							chatInput: chatInput,
-							prismaAIRSAction: action,
-							prompt_detected: prompt_detected,
-						}
-				});
-        
-      } catch (error: unknown) {
-			    let errorMessage = 'Unknown error';
-			    if (error instanceof Error) {
-			        // If the error is a standard Error object, use its message
-			        errorMessage = error.message;
-			    } else if (typeof error === 'object' && error !== null) {
-			        // If it's an object, stringify it to see its contents
-			        errorMessage = JSON.stringify(error);
-			    } else {
-			        // Otherwise, just use its string representation
-			        errorMessage = String(error);
-			    }
-			
-			    console.error('Prisma AIRS API Error:', errorMessage);
-			    returnData.push({
-			        json: {
-			            output: `Error calling Prisma AIRS API: ${errorMessage}`,
-			        },
-			    });
+		if (items[0].json.hasOwnProperty('chatInput') {
+			for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+				const sessionId = this.getNodeParameter('sessionId', itemIndex) as string;
+	      const chatInput = this.getNodeParameter('chatInput', itemIndex) as string;
+	      const aiProfileNameInput = this.getNodeParameter('aiProfileNameInput', itemIndex) as string;
+	
+	      const credentials = await this.getCredentials('prismaAIRSApi') as { apiKey: string };
+	      const apiKey = credentials.apiKey;
+	
+	      const requestOptions: IHttpRequestOptions = {
+	        method: 'POST' as IHttpRequestMethods,
+	        url: 'https://service.api.aisecurity.paloaltonetworks.com/v1/scan/sync/request',
+	        headers: {
+	          'Content-Type': 'application/json',
+	          'x-pan-token': apiKey,
+	        },
+	        body: {
+	          tr_id: sessionId,
+	          ai_profile: {
+	            profile_name: aiProfileNameInput,
+	          },
+	          metadata: {
+	            app_name: 'n8n',
+	            app_user: sessionId,
+	          },
+	          contents: [
+	            {
+	              prompt: chatInput,
+	            },
+	          ],
+	        },
+	        json: true, // Automatically parse JSON response
+	      };
+	
+	      try {
+	        // Pass message to Prisma AIRS API Intercept for inspection
+	        const response = await this.helpers.httpRequest(requestOptions);
+	
+	        // Process the AIRS response
+					const action = response.action;
+					const prompt_detected = response.prompt_detected;
+					returnData.push({
+							json: {
+								sessionId: sessionId,
+								chatInput: chatInput,
+								prismaAIRSAction: action,
+								prompt_detected: prompt_detected,
+							}
+					});
+					return this.prepareOutputData(returnData);
+	        
+	      } catch (error: unknown) {
+				    let errorMessage = 'Unknown error';
+				    if (error instanceof Error) {
+				        // If the error is a standard Error object, use its message
+				        errorMessage = error.message;
+				    } else if (typeof error === 'object' && error !== null) {
+				        // If it's an object, stringify it to see its contents
+				        errorMessage = JSON.stringify(error);
+				    } else {
+				        // Otherwise, just use its string representation
+				        errorMessage = String(error);
+				    }
+				
+				    console.error('Prisma AIRS API Error:', errorMessage);
+				    returnData.push({
+				        json: {
+				            output: `Error calling Prisma AIRS API: ${errorMessage}`,
+				        },
+				    });
+				}
 			}
 		}
     return this.prepareOutputData(returnData);
